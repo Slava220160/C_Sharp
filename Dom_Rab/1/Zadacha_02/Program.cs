cb2acbc80ec7ca4b37b28e10228de5e2759eefae
Slava <s220160@gmail.com>
2022-09-11 18:26:34 +0600

    add Zadacha_02 projeckt

---
+++ Dom_Rab/1/Zadacha_02/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 2
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+using static System.Console;
+Clear();
+
+WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32 (ReadLine());
+WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32 (ReadLine());
+
+if(number1>number2)
+{
+    Write($"Максимальное число {number1}");
+}
+if(number1 == number2)
+{
+    Write("Числа равны");
+}
+if(number1<number2)
+{
+    Write($"Максимальное число {number2}");
+}
+
+WriteLine();
